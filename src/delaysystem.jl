6156d15b70a70b5e3ea9aa89abebb7bb7ab44ae9
xiaoming <palmtree2013@163.com>
2021-12-24 13:44:23 +0800

    Update delaysystem.jl

--- src/delaysystem.jl
+++ src/delaysystem.jl
@@ -16,5 +16,16 @@ mutable struct DelayJumpSet
     delay_trigger_set::Vector{Int}
     delay_interrupt_set::Vector{Int}
 end
+"""
+    function(delay_trigger,delay_complete,delay_interrupt)
+
+The input arguments are
+
+-`delay_trigger::Dict{Int,Any}`     #  those reactions in the Markovian part that trigger the change of the state of the delay channels or/and the state of the reactants upon initiation. Keys: Indices of reactions defined in the Markovian part that can trigger the delay reactions; Values: Update functions(or `Pair` type) that decide how to update the delay channel or the state of the reactants.
+
+-`delay_interrupt::Dict{Int,Any}`   # those reactions in the Markovian part that change the state of the delay channels or/and the state of the reactants in the middle of on-going delay reactions. Keys: Indices of reactions defined in the Markovian part that can interrupt the delay reactions; Values: Update functions(or `Pair` type) that decide how to update the delay channel or the state of the reactants.
+
+-`delay_complete::Dict{Int,Any}`    # those reactions that are initiated by delay trigger reactions and change the state of the delay channels or/and the state of the reactants upon completion. Keys: Indices of the delay channel; Values: Update functions(or `Pair` type) that decide how to update the delay channel or the state of the reactants.
+"""
 DelayJumpSet(delay_trigger,delay_complete,delay_interrupt) = DelayJumpSet(delay_trigger,delay_complete,delay_interrupt, collect(keys(delay_trigger)), collect(keys(delay_interrupt)))
 
